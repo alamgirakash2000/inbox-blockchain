a7900b18af26f3e10f1a50c1cdd92ea80f33473b
alamgirakash2000 <alamgirakash2000@gmail.com>
2021-06-28 20:51:26 +0600

    final

--- deploy.js
+++ deploy.js
@@ -1,29 +1,22 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
+require('dotenv').config()
 
-const mnemonic = 'furnace ripple plunge snow direct author extra sample very case bike payment';
-const rinkebyEnd = 'https://rinkeby.infura.io/v3/15ebeb02363149d697d492aabeb7a6cf'
-
-const provider = new HDWalletProvider(
-   {
-      mnemonic: mnemonic,
-      providerOrUrl: rinkebyEnd,
-      addressIndex: 1
-    }
-)
+const provider = new HDWalletProvider(process.env.MNEMONIC, process.env.rinkebyEnd)
 const web3 = new Web3(provider);
 
-
+console.log(process.env.MNEMONIC, process.env.RINKEBYEND);
 
 const deploy = async () => {
    const accounts = await web3.eth.getAccounts();
 
    console.log("Attempting to deploy from account:", accounts[0]);
    
- let result = await new web3.eth.Contract(JSON.parse(interface))
-   .deploy({ data: bytecode, arguments: ["Hi there!"] })
-   .send({ from: accounts[0], gas:'1000000' })
-   console.log('Contract deployed to : ', result.options.address);
+   let result = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data: bytecode, arguments: ["Hi there!"] })
+      .send({ from: accounts[0], gas: '1000000' })
+      .catch(err => console.log(err.message));
+   console.log('Contract deployed to : ', result?.options.address);
 }
 deploy();
